ae930b0a4350ff570ed18988339f369b4c41b752
Carolina Cordoba <carolinacordoba.rg@gmail.com>
2025-12-28 19:57:08 +0000

    created the about.tsx component with its content

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -1,15 +1,28 @@
+import { getLocaleContent } from "@/app/lib/i18n";
+import Image from "next/image";
+
 interface AboutProps {
   locale: string;
 }
 export default async function About({ locale }: AboutProps) {
+  const t = getLocaleContent(locale);
+
     return (
-      <section id="about" className="min-h-screen bg-bg-soft p-8">
-        <h1 className="font-heading text-4xl text-text-primary mb-4">
-          {locale === 'sv' ? 'Om mig' : 'About'}
+      <section id="about" className="flex max-w-7xl mx-auto px-6">
+
+          {/* Text section */}
+          <div className="flex-1 flex-col justify-center">
+        <h1 className="font-heading text-[6rem] text-text-primary mb-4">
+          {t.about.title}
         </h1>
         <p className="font-body text-base text-text-secondary">
-          About content will go here.
+          {t.about.description}
         </p>
+        </div>
+        {/* Image section */}
+        <div className="flex-1 justify-center items-center ">
+          <Image src={"/images/phonemockup.png"} alt="Mobile phone with portrait" width={1000} height={1000} />
+        </div>
       </section>
     );
   }
