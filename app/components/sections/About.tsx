7a33991e6975c3dbfd317f10ae61247bed2b970f
Carolina Cordoba <carolinacordoba.rg@gmail.com>
2025-12-28 20:17:45 +0000

    fixed about section w correct w & h

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -11,17 +11,18 @@ export default async function About({ locale }: AboutProps) {
       <section id="about" className="flex max-w-7xl mx-auto px-6">
 
           {/* Text section */}
-          <div className="flex-1 flex-col justify-center">
-        <h1 className="font-heading text-[6rem] text-text-primary mb-4">
+          <div className="flex-3 flex flex-col">
+        <h1 className="font-heading text-[7rem] text-text-primary">
           {t.about.title}
         </h1>
-        <p className="font-body text-base text-text-secondary">
+        <hr className="top-0 mb-6 border-t w-1/2 border-text-secondary" />
+        <p className="font-body text-2xl text-text-primary">
           {t.about.description}
         </p>
         </div>
         {/* Image section */}
-        <div className="flex-1 justify-center items-center ">
-          <Image src={"/images/phonemockup.png"} alt="Mobile phone with portrait" width={1000} height={1000} />
+        <div className="relative flex-2 h-full items-center">
+          <Image src={"/images/phonemockup.png"} alt="Mobile phone with portrait" width={1000} height={1000} quality={90}/>
         </div>
       </section>
     );
